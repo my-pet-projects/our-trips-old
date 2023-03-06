0bec508cf9dafae569d161bfe5c7ade862183b43
see-sharp-dev <gleb.filimonov@sap.com>
2023-03-06 16:27:10 +0100

    add basic geo routes

---
+++ src/server/api/routers/geography.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+export const geographyRouter = createTRPCRouter({
+  getCountries: publicProcedure.query(async ({ ctx }) => {
+    return await ctx.prisma.country.findMany({});
+  }),
+  getCities: publicProcedure
+    .input(z.object({ countryCode: z.string() }))
+    .query(async ({ ctx, input }) => {
+      return await ctx.prisma.city.findMany({
+        where: {
+          countryCode: input.countryCode,
+        },
+      });
+    }),
+});
