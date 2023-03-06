ca1de8b8e1387463849ae3173b44533fdb10370e
see-sharp-dev <gleb.filimonov@sap.com>
2023-03-06 14:16:33 +0100

    change project configs

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,7 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  plugins: [
+    require.resolve("prettier-plugin-organize-imports"),
+    require.resolve("prettier-plugin-tailwindcss"),
+  ],
 };
